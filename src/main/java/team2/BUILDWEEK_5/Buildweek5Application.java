d932ccfd2fefdb1a19e4b345d508f2dff55f4a79
Giada <tonni.giada.210802@gmail.com>
2026-02-23 10:39:26 +0100

    Initial commit

---
+++ src/main/java/team2/BUILDWEEK_5/Buildweek5Application.java
@@ -0,0 +1,13 @@
+package team2.BUILDWEEK_5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Buildweek5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Buildweek5Application.class, args);
+	}
+
+}
---
+++ src/test/java/team2/BUILDWEEK_5/Buildweek5ApplicationTests.java
@@ -0,0 +1,13 @@
+package team2.BUILDWEEK_5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Buildweek5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
