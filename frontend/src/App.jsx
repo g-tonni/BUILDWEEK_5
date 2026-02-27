710c6769b5d42a5f280388f9247172d5e34f8694
LuxS94 <simonetti.lucio@libero.it>
2026-02-27 10:14:54 +0100

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter,Routes,Route} from 'react-router-dom'
+import './App.css'
+import MyForm from './assets/components/MyForm'
+import MyFormR from './assets/components/MyFormR'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MyNav from './assets/components/MyNav'
+import Home from './assets/components/Home'
+
+
+function App() {
+
+
+  return (
+    <>
+    <BrowserRouter>
+    <MyNav/>
+    <Routes>
+      <Route path="/login" element={<MyForm/>} />
+       <Route path="/signUp" element={<MyFormR/>} />
+        <Route path="/home" element={<Home/>} />
+    </Routes>
+    </BrowserRouter>
+      
+      
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/assets/components/Home.jsx
@@ -0,0 +1,4 @@
+function Home (){
+    return(<div  style={{marginTop:"200px"}}><h1 className="text-center" style={{fontSize:"100px"}}>Welcome to Epic Energy</h1></div>)
+}
+export default Home;
